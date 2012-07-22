41172f4aa7030e0c471ba32d28bf056b0f0cd3b6
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-22 17:23:51 +0000

    1.创建install目录(安装目录) 2.创建sql目录(sql文件目录) 3.创建sql文件
    本次sql更新:
    1.创建数据库t_info_together
    2.创建t_dairy表(日志表)
    3.创建t_drycomm表(日志评论表)
    4.之前在家写的baidu_news表(百度新闻表)

---
+++ trunk/install/sql/db.sql
@@ -0,0 +1,82 @@
+#
+# t_info_together
+# version:
+# time:
+#
+
+
+#----------------------------------------创建数据库---------------------------------------------------
+
+#数据库创建
+CREATE DATABASE `t_info_together` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+#----------------------------------------End(创建数据库)---------------------------------------------------
+
+
+#----------------------------------------日志模块---------------------------------------------------
+
+#日志表
+DROP TABLE IF EXISTS t_dairy;
+CREATE TABLE t_dairy
+(
+	dry_id		INT		NOT NULL	AUTO_INCREMENT,
+	dry_uid		INT		NOT NULL,
+	dry_title	VARCHAR(24)	NOT NULL,
+	dry_content	TEXT		NULL,
+	dry_pubtime	INT		NOT NULL,
+	dry_lmoditime	INT		NOT NULL,
+	dry_ifcomm	BOOLEAN		NOT NULL	DEFAULT 0,
+	dry_private	BIT(2)		NOT NULL	DEFAULT 0,
+	PRIMARY KEY(dry_id)
+)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM;
+
+#日志表(通过phpMyAdmin手工创建的表转化为的SQL)
+CREATE TABLE `t_info_together`.`t_dairy` (
+`dry_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`dry_uid` INT NOT NULL ,
+`dry_title` VARCHAR( 24 ) NOT NULL ,
+`dry_content` TEXT NULL ,
+`dry_pubtime` INT NOT NULL ,
+`dry_lmoditime` INT NOT NULL ,
+`dry_ifcomm` BOOL NOT NULL DEFAULT '0',
+`dry_private` BIT( 2 ) NOT NULL DEFAULTb '0'
+) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+#日志评论表
+DROP TABLE IF EXISTS t_drycomm;
+CREATE TABLE t_drycomm
+(
+	drycm_id		INT		NOT NULL	AUTO_INCREMENT,
+	drycm_dryid		INT		NOT NULL,
+	drycm_uid		INT		NOT NULL,
+	drycm_title		VARCHAR(24)	NULL,
+	drycm_content		TEXT		NULL,
+	drycm_pubtime		INT		NOT NULL,
+	drycm_ifcomm		BOOLEAN		NOT NULL	DEFAULT 0,
+	drycm_fathercmid	TINYINT		NOT NULL	DEFAULT 0,
+	PRIMARY KEY(drycm_id)
+)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM;
+
+#----------------------------------------End(日志模块)---------------------------------------------------
+
+
+
+
+#-----------------------------------------百度官方新闻(Over)---------------------------------------------------------
+
+
+DROP TABLE IF EXISTS baidu_news;
+CREATE TABLE baidu_news (
+  id INT(10) unsigned NOT NULL auto_increment,
+  title varchar(100) NOT NULL,
+  content text,
+  publish_time INT(10) NOT NULL,
+  #site_from_name varchar(20) NOT NULL DEFAULT '',
+  news_from_url varchar(60) NOT NULL DEFAULT '',
+  ifread INT(1) NOT NULL,
+  comment text DEFAULT '',
+  PRIMARY KEY(id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+#创建时间	2012年7月6日 13:08:46
+
+#-----------------------------------------End(百度官方新闻)------------------------------------------------------------
