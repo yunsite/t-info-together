e34974105a4a4577ca4e2a79812b1ddd5cd0972d
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-25 23:42:45 +0000

    增加"ShuoShuo.php" Model文件(增加ShuoShuo类),添加函数注释: 1.添加"说说" 2.删除"说说" 3.删除"说说"评论 4.修改"说说" 5.查询"说说" 6.转发"说说" 7.评论"说说" 8.赞"说说"

---
+++ trunk/mine/models/ShuoShuo.php
@@ -0,0 +1,77 @@
+<?php
+
+	/*
+	*	Description:	"说说"功能处理
+	*
+	*
+	*/
+	
+	class ShuoShuo{
+	
+
+			/*	()
+			*	@Description:	添加"说说"
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除"说说"
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除"说说"评论
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	修改"说说"
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	查询"说说"
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	转发"说说"
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	评论"说说"
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	赞"说说"
+
+				@param	
+			*
+			*
+			*/
+
+	}
+?>
