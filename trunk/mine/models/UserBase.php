813f37b61ff66046d5ac99c8d48e5c81ca62b1a3
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-25 05:06:49 +0000

    增加UserBase.php(用户基本处理)模型文件,加了一些函数注释,注明该模型文件应该有哪些动作Action

---
+++ trunk/mine/models/UserBase.php
@@ -0,0 +1,64 @@
+<?php
+
+	/*
+	*	Description:	用户基本处理模型
+	*
+	*
+	*/
+	
+			/*	()
+			*	@Description:	注册(增加)用户
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除用户
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	修改用户基本信息
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	查询用户基本信息
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	用户登陆
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	用户注销
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	忘记密码
+
+				@param	
+			*
+			*
+			*/
+?>
