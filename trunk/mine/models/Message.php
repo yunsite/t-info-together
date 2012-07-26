483000f89848dd68e247374994c7d3806925a4f4
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-26 05:07:15 +0000

    增加Message(消息处理模型),添加函数功能注释(以后实现): 1.发送消息 2.删除消息 3.查询消息

---
+++ trunk/mine/models/Message.php
@@ -0,0 +1,38 @@
+<?php
+
+	/*
+	*	Description:	消息处理模型
+	*
+	*
+	*/
+	
+	class Message{
+	
+
+			/*	()
+			*	@Description:	发送消息
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除消息
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	查询消息
+
+				@param
+			*
+			*
+			*/
+
+
+	}
+?>
