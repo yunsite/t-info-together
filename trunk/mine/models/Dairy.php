bcf751a96d3f007b78e8621d825f6ae6d0069363
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-26 05:01:37 +0000

    增加Dairy日志处理模型: 1添加日志 2删除日志 3修改日志 4查询日志 5添加日志评论 6删除评论 7添加日志分类 8删除日志分类 9修改日志分类

---
+++ trunk/mine/models/Dairy.php
@@ -0,0 +1,85 @@
+<?php
+
+	/*
+	*	Description:	日志处理模型
+	*
+	*
+	*/
+	
+	class Dairy{
+	
+
+			/*	()
+			*	@Description:	添加日志
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除日志
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	修改日志
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	查询日志
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	添加日志评论
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除评论
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	添加日志分类
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除日志分类
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	修改日志分类
+
+				@param	
+			*
+			*
+			*/
+
+	}
+?>
