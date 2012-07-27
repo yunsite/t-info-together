a514a9e4b90deca6aa024b62e8e3816592422f1e
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-27 00:34:05 +0000

    添加Friends.php(好友处理模型) 添加函数功能注释: 1.添加好友 2.删除好友 3.查询好友

---
+++ trunk/mine/models/Friends.php
@@ -0,0 +1,37 @@
+<?php
+
+	/*
+	*	Description:	好友处理模型
+	*
+	*
+	*/
+	
+	class Friends{
+	
+
+			/*	()
+			*	@Description:	添加好友
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	删除好友
+
+				@param	
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	查询好友
+
+				@param
+			*
+			*
+			*/
+
+	}
+?>
