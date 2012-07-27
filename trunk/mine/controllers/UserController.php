cdf7802678613b4a878ebb92f76239b9dc241ff6
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-27 15:07:22 +0000

    添加文件:mine/controllers/UserController.php(用户处理控制器),并添加功能方法注释

---
+++ trunk/mine/controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+	/*
+	*	Description:	用户处理控制器
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	注册用户
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	添加用户(管理员后台添加)
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	删除用户(管理员后台删除)
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	修改用户信息
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	找回密码
+
+		@param	none
+	*
+	*
+	*/
+
+?>
