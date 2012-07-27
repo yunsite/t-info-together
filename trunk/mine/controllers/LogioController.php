5c416007f9c0f334679f4c7a5501d0a05883d8f9
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-27 15:02:17 +0000

    添加文件:mine/controllers/LogioController.php(登陆,登出控制器), 添加方法功能注释

---
+++ trunk/mine/controllers/LogioController.php
@@ -0,0 +1,24 @@
+<?php
+	/*
+	*	Description:	登陆,登出控制器
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	登陆系统
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	登出系统
+
+		@param	none
+	*
+	*
+	*/
+
+?>
