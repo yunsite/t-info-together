da549d05e2db5d98990b20df78a2c5726d7f59ad
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-07-28 21:25:13 +0000

    增加控制器: mine/controllers/DairyController.php 添加函数功能注释: 1.添加日志 2.删除日志 3.修改日志(内容,分类,隐私) 4.查询日志

---
+++ trunk/mine/controllers/DairyController.php
@@ -0,0 +1,42 @@
+<?php
+	/*
+	*	Description:	日志控制器
+	*
+	*
+	*/
+
+//检测程序安装与否,如果没安装的话,跳转到install/index.php(安装程序入口文件)
+	
+	/*	()
+	*	@Description:	添加日志
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	删除日志
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	修改日志(内容,分类,隐私等)
+
+		@param	none
+	*
+	*
+	*/
+
+	/*	()
+	*	@Description:	查询日志
+
+		@param	none
+	*
+	*
+	*/
+
+?>
