48a05ef53380373681c3a6ac06849221bce2ab0d
jiazhaoteng <jiazhaoteng@gmail.com>
2012-08-01 15:09:29 +0000

    增加模型: 增加 站点基本信息模型

---
+++ mine/models/SiteInfo.php
@@ -0,0 +1,29 @@
+<?php
+
+	/*
+	*	Description:	站点基本信息模型
+	*
+	*
+	*/
+	
+	class SiteInfo{
+	
+
+			/*	()
+			*	@Description:	修改站点基本信息
+
+				@param
+			*
+			*
+			*/
+
+			/*	()
+			*	@Description:	查询站点基本信息
+
+				@param	
+			*
+			*
+			*/
+
+	}
+?>
