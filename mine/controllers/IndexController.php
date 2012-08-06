b9153bca58d0afc102b2edbbe532be64da661f88
jiazhaoteng <jiazhaoteng@gmail.com>
2012-08-06 22:37:50 +0000

    修订: 修改 IndexController默认控制器 的构造函数,使之可以默认访问 IndexAction默认动作

--- mine/controllers/IndexController.php
+++ mine/controllers/IndexController.php
@@ -14,8 +14,17 @@
 			//一般情况下,如果直接通过URL方式访问该处理脚本,则之前不会包含 系统初始处理脚本 ,通过下面语句来包含,而通过index.php方式访问则可以包含
 			include_once("../global.php");
 
-			//调用默认Action方法(初始动作)显示首页
-			$this->IndexAction();
+			//无参调用 IndexController控制器
+			if( empty($arg_get) && empty($arg_post) ){
+			
+				//调用默认Action方法(初始动作)显示首页
+				$this->IndexAction();
+
+				//交给默认控制器显示模板后,就不再需要显示其他Action方法的模板了,所以结束代码
+				die();
+
+			}
+			
 
 			//下面被注释掉的if...else...语句结构是程序设计模式的一个示范
 			/*
