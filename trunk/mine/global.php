d00b2b6acc47163ca6bacfa7f71fab2953b3e326
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-08-09 05:05:50 +0000

    修改: 把 站点基本信息 获取处理,从 IndexController 转移到 global 文件里

--- trunk/mine/global.php
+++ trunk/mine/global.php
@@ -68,4 +68,109 @@
 
 
 	//检测当前脚本
+
+
+
+	//获取系统基本信息
+
+	//判断缓存是否存在,而读取缓存
+	if( file_exists("data/cache/SiteInfo.cache") ){
+				
+		//echo "缓存文件存在!";
+		//echo "<br/>";
+
+		$siteinfo_new =  read_cache("data/cache/SiteInfo.cache");
+		//print_r($siteinfo_new);
+
+		//读取缓存文件相关信息;
+
+		//赋值所需要的变量;
+
+		//$sys_title = ;
+
+		//$sys_keywords = ;
+
+		//$sys_description = ;
+
+	}else{
+			
+		//声明变量为全局变量
+		//global $db_server, $db_name, $db_user, $db_pwd, $sys_charset;
+
+		//创建数据库对象(在需要的时候才创建,而不是系统一初始化就创建,因为系统有缓存机制,如果没特殊操作,如系统后台更新了缓存,则使用缓存)
+		//似乎在后面直接声明它的子类对象就行了,不清楚的是,子类对象创建的时候会不会调用父类对象的构造函数(db_class会自动连接数据库),记得好像是会
+		//$db_base = new db_class( $db_server, $db_name, $db_user, $db_pwd, $sys_charset );
+				
+		//print_r( $db_base );
+
+		//包含 model,在这里检测包含处理用到的model
+		//@include_once("../models/SiteInfo.php");
+		include_once("models/SiteInfo.php");
+
+		/*if( 是直接访问 ){
+				
+			include_once("../models/SiteInfo.php");
+
+		}else{//通过index.php入口文件访问
+				
+			include_once("models/SiteInfo.php");
+
+		}
+		*/
+
+		$db_base = new SiteInfo( $db_server, $db_name, $db_user, $db_pwd, $sys_charset );
+
+		//print_r( $db_base );
+
+		//查询站点基本信息
+		$siteinfo = $db_base -> si_select( '*' ,'' );
+
+		//print_r( $siteinfo );
+				
+		//遍历赋值 站点信息
+		foreach( $siteinfo as $key => $value ){
+				
+			switch( $value["conf_name"] ){
+						
+				//系统名
+				case "site_name": $siteinfo_new["site_name"] = $value["conf_value"];
+				break;
+
+				//关键字
+				case "site_keywords": $siteinfo_new["site_keywords"] = $value["conf_value"];
+				break;
+
+				//站点描述
+				case "site_description": $siteinfo_new["site_description"] = $value["conf_value"];
+				break;
+					
+			}
+				
+		}
+				
+		//echo $sys_description;
+
+		/*
+		//整合站点信息为数组,便于缓存和模板输出
+		$siteinfo_new["site_name"] = $sys_title;
+		$siteinfo_new["site_keywords"] = $sys_keywords;
+		$siteinfo_new["site_description"] = $sys_description;
+		*/
+
+				
+
+		/*
+		$
+
+		//写缓存操作(如果缓存机制开启的话)
+		if( 开启缓存机制 ){
+				
+			写缓存操作;
+
+		}
+		*/
+		//这里暂未考虑缓存机制开启选项(需要修改缓存的内容——缓存指定的信息)
+		write_cache( 'data/cache/SiteInfo.cache', $siteinfo_new );
+	}
+
 ?>
