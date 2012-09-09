b360bf3e4e4fad291d12ec516a11ef86de85e586
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-09-09 05:18:13 +0000

    修改:删除注释(最近休息眼睛,有段时间没有更新代码了)

--- trunk/mine/controllers/UserCenter/DairyController.php
+++ trunk/mine/controllers/UserCenter/DairyController.php
@@ -3,17 +3,7 @@
 	*	Description:	日志控制器
 	*
 	*
-	*/
-
-//检测程序安装与否,如果没安装的话,跳转到install/index.php(安装程序入口文件)
-	
-	/*	()
-	*	@Description:	添加日志
-
-		@param	none
-	*
-	*
-	*/
+	*/	
 
 	/*	()
 	*	@Description:	删除日志
