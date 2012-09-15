1fe2e236c263c06aea4c9bd37a388c570a76af31
jiazhaoteng <jiazhaoteng@gmail.com>
2012-09-15 23:53:20 +0000

    修订: Dairy.php 完善 增加日志方法 private function add_dairy( $dry_uid, $dry_title, $dry_content, $dry_ifcomm = 0, $dry_private = 0 )

--- mine/models/Dairy.php
+++ mine/models/Dairy.php
@@ -13,11 +13,34 @@
 			*	@Description:	添加日志
 
 				@param
+						$dry_uid	 用户id
+						$dry_title	 日志title
+						$dry_content 日志content
+						$dry_ifcomm		是否允许评论	|	0(允许)
+						$dry_private	日志隐私设置	|	0(所有人可见)
+
 			*
 			*
 			*/
-			private function add_dairy(){
+			private function add_dairy( $dry_uid, $dry_title, $dry_content, $dry_ifcomm = 0, $dry_private = 0 ){
 			
+				//日志发布时间
+				$dry_pubtime = time();
+
+				//添加日志时,最后修改时间跟发布时间是一样的
+				$dry_lmoditime = $dry_pubtime;
+
+				//构造 $coloumns	字段
+				$coloumns = "dry_uid,dry_title,dry_content,dry_pubtime,dry_lmoditime,dry_ifcomm,dry_private";
+
+				//构造 $values		值
+				$values = "'".$dry_uid."','".$dry_title."','".$dry_content."','".$dry_pubtime."','".$dry_lmoditime."','".$dry_ifcomm."','".$dry_private."'";
+
+				//增加日志
+				parent::db_insert( 't_dairy', $coloumns, $values );
+
+
+
 			}
 
 			/*	()
