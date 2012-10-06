459bde2e26498be62a3b50b338ae3f1108977a17
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2012-10-06 04:14:56 +0000

    增加: 1.增加 —— paging( $page, $total, $phpfile, $pagesize, $pagelen ) 自动适应范围的页码分页函数

--- trunk/mine/includes/function.php
+++ trunk/mine/includes/function.php
@@ -336,4 +336,100 @@
 	}
 
 
+	/*
+	*
+	*	@Description:	自动适应范围的页码分页函数
+
+						效果如下:
+						<< < 1 2 3 4 5 6 7 > >>
+
+						<< < 6 7 8 9 10 11 12 > >>
+
+
+	*	@Param	$page		访问的页码
+				$total		记录总数
+				$phpfile	分页处理访问的文件名
+				$pagesize	每页的最大记录数
+				$pagelen	显示的页码数目
+	*	@Return Array( pagecode, sqllimit )
+	*
+	*
+	*/
+	function paging( $page, $total, $phpfile, $pagesize, $pagelen ){
+
+		$pagecode = '';						//定义变量,存放分页生成的HTML
+		$page = intval($page);				//避免非数字页码
+		$total = intval($total);			//保证总记录数值类型正确
+
+		if( !$total )						//总记录数为零返回空数组
+			return array();
+
+		$pages = ceil( $total/$pagesize );	//计算总分页
+
+		//处理页码合法性
+		if( $page<1 )
+			$page = 1;
+		if( $page>$pages )
+			$page = $pages;
+
+		//计算查询偏移量
+		$offset = $pagesize*( $page-1 );
+
+		//页码范围计算
+		$init = 1;											//起始页码数
+		$max = $pages;										//结束页码数
+		$pagelen = ( $pagelen%2 ) ? $pagelen:$pagelen+1;	//页码个数
+		$pageoffset = ($pagelen-1)/2;						//页码个数左右偏移量
+
+		//生成html
+		$pagecode='<div class="page">';
+		$pagecode.="<span>$page/$pages</span>";				//第几页,共几页
+
+		//如果是第一页，则不显示第一页和上一页的连接
+		if($page!=1){
+			$pagecode.="<a href="{$phpfile}?page=1"><<</a>";			//第一页
+			$pagecode.="<a href="{$phpfile}?page=".($page-1).""><</a>";	//上一页
+		}
+
+		if($pages>$pagelen){//分页数大于页码个数时可以偏移
+		
+			if($page<=$pageoffset){//如果当前页小于等于左偏移
+				$init=1;
+				$max = $pagelen;
+			}else{//如果当前页大于左偏移
+			
+				if($page+$pageoffset>=$pages+1){//如果当前页码右偏移超出最大分页数
+					$init = $pages-$pagelen+1;
+
+				}else{//左右偏移都存在时的计算
+				
+					$init = $page-$pageoffset;
+					$max = $page+$pageoffset;
+				}
+
+			}
+
+		}
+
+		//生成html
+		for($i=$init;$i<=$max;$i++){
+
+			if($i==$page){
+				$pagecode.='<span>'.$i.'</span>';
+			}else{
+				$pagecode.="<a href="{$phpfile}?page={$i}">$i</a>";
+			}
+		}
+		
+		if($page!=$pages){
+			$pagecode.="<a href="{$phpfile}?page=".($page+1)."">></a>";		//下一页
+			$pagecode.="<a href="{$phpfile}?page={$pages}">>></a>";			//最后一页
+		}
+
+		$pagecode.='</div>';
+		return array('pagecode'=>$pagecode,'sqllimit'=>' limit '.$offset.','.$pagesize);
+	}
+
+
+
 ?>
