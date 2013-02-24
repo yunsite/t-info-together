cca33d4cbb7145464e7aadb7a7dfcb1115c4f1ac
jiazhaoteng <jiazhaoteng@gmail.com>
2013-02-24 13:37:54 +0000

    修订: 整理 config.php配置文件 ,使之更可读,对开发者更友好

--- mine/config.php
+++ mine/config.php
@@ -18,6 +18,11 @@
 	//系统编码
 	$sys_charset = 'utf8';
 
+	//Cookie加密密钥
+	$Cookie_pwd = 't_info_together';
+
+//============================数据库配置==================================
+
 	//数据库连接地址
 	$db_server = '127.0.0.1';
 
@@ -27,51 +32,12 @@
 	//数据库用户名
 	$db_user = 'root';
 
-
 	//数据库密码
 	$db_pwd = '';
 
-	//Cookie加密密钥
-	$Cookie_pwd = 't_info_together';
+//============================End(数据库配置)==============================
 
-	//Smarty模板配置变量
-
-	//Smarty配置,暂时先不用绝对路径情况,而只针对index.php入口文件写下配置
-	/*
-	//模板目录
-	$sys_template_dir = "/templates/";
-	//模板编译目录
-	$sys_compile_dir = "/data/templates_c/";
-	//特殊配置文件目录
-	$sys_config_dir = "";
-	//缓存目录
-	$sys_cache_dir = "/data/cache/";
-	//是否允许缓存
-	$sys_caching = 0;
-	//替换变量左边界符
-	$sys_left_delimiter = "<{";
-	//替换变量右边界符
-	$sys_right_delimiter = "}>";
-	*/
-	$sys_template_dir = $sys_dir."templates/";
-
-	$sys_compile_dir = $sys_dir."data/templates_c/";
-
-	$sys_config_dir = "";
-
-	$sys_cache_dir = $sys_dir."data/cache/";
-
-	$sys_caching = 0;
-
-	$sys_left_delimiter = "<{";
-
-	$sys_right_delimiter = "}>";
-
-
-
-
-	//邮件服务器配置
-
+//============================邮件服务器配置==================================
 	//邮件服务器地址
 	$smtp_host = "smtp.qq.com";
 
@@ -92,6 +58,7 @@
 
 	//邮件Encoding(暂为base64,至于为什么是它,我也不太清楚,稍后查一下)
 	$mail_encoding = "base64";
+//============================End(邮件服务器配置)==============================
 
 //==============================Smarty配置====================================
 	//包含	Smarty类文件
@@ -99,13 +66,20 @@
 	
 	//Smarty模板配置
 	$tpl = new Smarty();
-	$tpl->template_dir = $sys_template_dir;
-	$tpl->compile_dir = $sys_compile_dir;
-	$tpl->config_dir = $sys_config_dir;
-	$tpl->cache_dir = $sys_cache_dir;
-	$tpl->caching = $sys_caching;
-	$tpl->left_delimiter = $sys_left_delimiter;
-	$tpl->right_delimiter = $sys_right_delimiter;
+	//模板目录
+	$tpl->template_dir = $sys_dir."templates/";
+	//模板编译目录
+	$tpl->compile_dir = $sys_dir."data/templates_c/";
+	//特殊配置文件目录
+	$tpl->config_dir = "";
+	//缓存目录
+	$tpl->cache_dir = $sys_dir."data/cache/";
+	//是否允许缓存
+	$tpl->caching = 0;
+	//替换变量左边界符
+	$tpl->left_delimiter = "<{";
+	//替换变量右边界符
+	$tpl->right_delimiter = "}>";
 
 //==============================End(Smarty配置)====================================
 
