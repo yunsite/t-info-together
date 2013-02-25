9200f79508ac48fd92c99cd14ccf9e21f9c35768
jiazhaoteng@gmail.com <jiazhaoteng@gmail.com@828f2abf-b498-9435-b51e-207ed7d64b51>
2013-02-25 06:48:06 +0000

    修订: 1.index.php —— 整理代码为独立URI控制做准备 2.mine目录及下面文件 —— 去除版本化,重新布置结构,以符合T-framework的URI规则

--- trunk/mine/index.php
+++ trunk/mine/index.php
@@ -8,17 +8,6 @@
 	//包含 系统初始化处理文件
 	include_once("global.php");
 
-	//可以在index.php,增加使用CI模式的url控制传递参数方式(通过检测get方式传递过来的参数)(该功能暂未加)
-
-
-	//example.com/news/article/my_article
-
-	//根据模型-视图-控制器模式，在此 URL 段一般以如下形式表示：
-	//example.com/class/function/ID
-	//第一段表示调用控制器类。
-	//第二段表示调用类中的函数或方法。
-	//第三及更多的段表示的是传递给控制器的参数，如 ID 或其它各种变量。
-
 	//print_r($_COOKIE);	//有AJSTAT_ok_times的Cookie字段
 	
 	/*
@@ -27,7 +16,6 @@
 	echo date( "Y/m/d G:i:s", "1352300488" );	//2012/11/07 15:01:28 (现在时间为2012年11月7日 23:03:33)
 	*/
 
-	//根据参数判断处理交由哪个控制器处理
 	if( isset($_GET['logio']) || isset($_GET['reg']) ){	//登陆,退出
 	
 		include_once("controllers/DoorController.php");
